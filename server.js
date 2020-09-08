019967bc995749b7535f4f750196a9fd12e585d1
Kadence Neuens <kadenceneuens@gmail.com>
2020-09-08 01:18:54 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET 
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
