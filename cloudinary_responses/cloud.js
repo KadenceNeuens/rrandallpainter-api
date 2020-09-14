41d6ec145d4335efc3fb75f50cad79892634c002
Kadence Neuens <kadenceneuens@gmail.com>
2020-09-14 15:02:22 -0500

    testing image api and upload

--- cloudinary_responses/cloud.js
+++ cloudinary_responses/cloud.js
@@ -1,6 +1,11 @@
 // GET Responses for Cloudinary
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+router.use(bodyParser.json()) // for parsing application/json
+const multer = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
 // Cloudinary media CDN
 const cloudinary = require('cloudinary')
@@ -17,7 +22,7 @@ const METADATA_URL = 'https://'+process.env.API_KEY+':'+process.env.API_SECRET+'
 // GET
 
 // All Images
-router.get('/images', (req, res) => {
+router.post('/images', (req, res) => {
     var data = req.body
     cloudinary.v2.search
     .expression('resource_type:image')
@@ -34,16 +39,15 @@ router.get('/images', (req, res) => {
 })
   
 // Residential Images
-router.get('/images/residential', (req, res) => {
+router.post('/images/residential', (req, res) => {
     var data = req.body
-    console.log("JSON Stringify:")
-    console.log(JSON.stringify(req.body, null, 2))
+    console.log(req.body)
     cloudinary.v2.search
     .expression('resource_type:image AND tags=residential')
     .execute().then( result => {
         var urls = []
         result.resources.map((item, index) => {
-            urls.push(cloudinary.image(item.filename+'.'+item.format, {data}))
+            urls.push(cloudinary.image(item.filename+'.'+item.format, {transformation: [...data]}))
             console.log(urls)
         })
         console.log(JSON.stringify(urls, null, 2))
@@ -58,20 +62,56 @@ router.get('/images/residential', (req, res) => {
 })
   
 // Commercial Images
-router.get('/images/commercial', (req, res) => {
+router.post('/images/commercial', (req, res) => {
     var data = req.body
     cloudinary.v2.search
     .expression('resource_type:image AND tags=commercial')
     .execute().then( result => {
         var urls = []
-        result.resources.map((item, index) => {
-            urls.push(cloudinary.image(item.filename+'.'+item.format, {data}))
-            console.log(urls)
-        })
+        if (!req.body)
+        {
+            result.resources.map((item, index) => {
+                urls.push(cloudinary.image(item.filename+'.'+item.format, {data}))
+                console.log(urls)
+            })
+        }
+        else
+        {
+            result.resources.map((item, index) => {
+                urls.push(cloudinary.image(item.filename+'.'+item.format))
+                console.log(urls)
+            })
+        }
         console.log(JSON.stringify(urls, null, 2))
         res.json(urls)
     });
     res.end("Returned commercial images")
+})
+
+const multiUpload = upload.array('images');
+
+const multiUploads = (req, res, next) => {
+  multiUpload(req, res, (error) => {
+    if (error) {
+      return res.sendApiError(
+        { title: 'Upload Error',
+          detail:  error.message});
+      }
+    next();
+  })
+}
+
+router.post('/api/upload', multiUploads, (req, res) => {
+    try {
+        if(!req.files) throw new Error("Image upload failed!")
+        req.files.map((item, index) => {
+            cloudinary.v2.uploader.upload(item[index])
+            console.log("Uploaded:" + item[index])
+        })
+        res.status(200).json({ message: "Success!" })
+    }
+    catch(error)
+    { res.status(422).json({ message: error.message }) }
 })
 
 module.exports = router
