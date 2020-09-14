03c3bd3abe724d9cd5d809549a2dc15eb8b12972
Shun Chiang <yuushamenma@gmail.com>
2020-09-14 18:53:06 -0500

    tests

--- cloudinary_responses/cloud.js
+++ cloudinary_responses/cloud.js
@@ -1,127 +1,139 @@
 // GET Responses for Cloudinary
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-router.use(bodyParser.json()) // for parsing application/json
-const multer = require('multer')
-var storage = multer.memoryStorage()
-var upload = multer({ storage: storage })
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+router.use(bodyParser.json()); // for parsing application/json
+const multer = require("multer");
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 // Cloudinary media CDN
-const cloudinary = require('cloudinary')
+const cloudinary = require("cloudinary");
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET 
-  });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
-const METADATA_URL = 'https://'+process.env.API_KEY+':'+process.env.API_SECRET+'@api.cloudinary.com/v1_1/'+process.env.CLOUD_NAME+'/metadata_fields'
+const METADATA_URL =
+  "https://" +
+  process.env.API_KEY +
+  ":" +
+  process.env.API_SECRET +
+  "@api.cloudinary.com/v1_1/" +
+  process.env.CLOUD_NAME +
+  "/metadata_fields";
 
 //-------------------------------------
 // GET
 
 // All Images
-router.post('/images', (req, res) => {
-    var data = req.body
-    cloudinary.v2.search
-    .expression('resource_type:image')
-    .execute().then( result => {
-        var urls = []
-        result.resources.map((item, index) => {
-            urls.push(cloudinary.image(item.filename+'.'+item.format, {data}))
-            console.log(urls)
-        })
-        console.log(JSON.stringify(urls, null, 2))
-        res.json(urls)
+router.post("/images", (req, res) => {
+  var data = req.body;
+  cloudinary.v2.search
+    .expression("resource_type:image")
+    .execute()
+    .then((result) => {
+      var urls = [];
+      result.resources.map((item, index) => {
+        urls.push(
+          cloudinary.image(item.filename + "." + item.format, { data })
+        );
+        console.log(urls);
+      });
+      console.log(JSON.stringify(urls, null, 2));
+      return res.json(urls);
     });
-    res.end("Returned all images")
-})
-  
+  //   res.end("Returned all images");
+});
+
 // Residential Images
-router.post('/images/residential', (req, res) => {
-    var data = req.body
-    console.log(req.body)
-    cloudinary.v2.search
-    .expression('resource_type:image AND tags=residential')
-    .execute().then( result => {
-        var urls = []
-        if (!req.body)
-        {
-            result.resources.map((item, index) => {
-                urls.push(cloudinary.image(item.filename+'.'+item.format, {transformation: [...data]}))
-                console.log(urls)
+router.post("/images/residential", (req, res) => {
+  var data = req.body;
+  console.log(req.body);
+  cloudinary.v2.search
+    .expression("resource_type:image AND tags=residential")
+    .execute()
+    .then((result) => {
+      var urls = [];
+      if (data !== undefined) {
+        result.resources.map((item, index) => {
+          urls.push(
+            cloudinary.image(item.filename + "." + item.format, {
+              transformation: [...data],
             })
-        }
-        else
-        {
-            result.resources.map((item, index) => {
-                urls.push(cloudinary.image(item.filename+'.'+item.format))
-                console.log(urls)
-            })
-        }
-        console.log(JSON.stringify(urls, null, 2))
-        res.json(urls)
+          );
+          console.log(urls);
+        });
+      } else {
+        result.resources.map((item, index) => {
+          urls.push(cloudinary.image(item.filename + "." + item.format));
+          console.log(urls);
+        });
+      }
+      console.log(JSON.stringify(urls, null, 2));
+      res.json(urls);
     });
 
-    // Fetching metadata for position (not working yet)
-    // fetch(METADATA_URL)
-    //   .then(res => res.json())
-    //   .then(json => console.log(json));
-    res.end("Returned residential images")
-})
-  
+  // Fetching metadata for position (not working yet)
+  // fetch(METADATA_URL)
+  //   .then(res => res.json())
+  //   .then(json => console.log(json));
+  //   res.end("Returned residential images");
+});
+
 // Commercial Images
-router.post('/images/commercial', (req, res) => {
-    var data = req.body
-    cloudinary.v2.search
-    .expression('resource_type:image AND tags=commercial')
-    .execute().then( result => {
-        var urls = []
-        if (!req.body)
-        {
-            result.resources.map((item, index) => {
-                urls.push(cloudinary.image(item.filename+'.'+item.format, {transformation: [...data]}))
-                console.log(urls)
+router.post("/images/commercial", (req, res) => {
+  var data = req.body;
+  cloudinary.v2.search
+    .expression("resource_type:image AND tags=commercial")
+    .execute()
+    .then((result) => {
+      var urls = [];
+      if (!req.body) {
+        result.resources.map((item, index) => {
+          urls.push(
+            cloudinary.image(item.filename + "." + item.format, {
+              transformation: [...data],
             })
-        }
-        else
-        {
-            result.resources.map((item, index) => {
-                urls.push(cloudinary.image(item.filename+'.'+item.format))
-                console.log(urls)
-            })
-        }
-        console.log(JSON.stringify(urls, null, 2))
-        res.json(urls)
+          );
+          console.log(urls);
+        });
+      } else {
+        result.resources.map((item, index) => {
+          urls.push(cloudinary.image(item.filename + "." + item.format));
+          console.log(urls);
+        });
+      }
+      console.log(JSON.stringify(urls, null, 2));
+      res.json(urls);
     });
-    res.end("Returned commercial images")
-})
+  res.end("Returned commercial images");
+});
 
-const multiUpload = upload.array('images');
+const multiUpload = upload.array("images");
 
 const multiUploads = (req, res, next) => {
   multiUpload(req, res, (error) => {
     if (error) {
-      return res.sendApiError(
-        { title: 'Upload Error',
-          detail:  error.message});
-      }
-    next();
-  })
-}
-
-router.post('/api/upload', multiUploads, (req, res) => {
-    try {
-        if(!req.files) throw new Error("Image upload failed!")
-        req.files.map((item, index) => {
-            cloudinary.v2.uploader.upload(item[index])
-            console.log("Uploaded:" + item[index])
-        })
-        res.status(200).json({ message: "Success!" })
+      return res.sendApiError({ title: "Upload Error", detail: error.message });
     }
-    catch(error)
-    { res.status(422).json({ message: error.message }) }
-})
+    next();
+  });
+};
 
-module.exports = router
+router.post("/api/upload", multiUploads, (req, res) => {
+  try {
+    if (!req.files) throw new Error("Image upload failed!");
+    req.files.map((item, index) => {
+      cloudinary.v2.uploader.upload(item[index]);
+      console.log("Uploaded:" + item[index]);
+    });
+    res.status(200).json({ message: "Success!" });
+  } catch (error) {
+    res.status(422).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,108 +1,102 @@
-const express = require('express')
-const app = express()
-const port = 3080
-const bodyParser = require('body-parser')
-app.use(bodyParser.json()) // for parsing application/json
+const express = require("express");
+const app = express();
+const port = 3080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); // for parsing application/json
 
-const cors = require('cors')
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+const cors = require("cors");
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-require('dotenv').config()
-const { Console } = require('console')
-const fetch = require('node-fetch')
+require("dotenv").config();
+const { Console } = require("console");
+const fetch = require("node-fetch");
 
 // My Cloudinary Middleware
-const cloud = require('./cloudinary_responses/cloud')
+const cloud = require("./cloudinary_responses/cloud");
 
 // Authentication & Sessions
-const auth = require('basic-auth')
-const compare = require('tsscmp')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const auth = require("basic-auth");
+const compare = require("tsscmp");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 // Session settings
 const sess = {
-  secret: 'big chungus',
+  secret: "big chungus",
   resave: false,
   saveUninitialized: true,
   cookie: {},
   store: new MongoStore({
     url: process.env.MONGO_URL,
-    ttl: 120
-  })
-}
+    ttl: 120,
+  }),
+};
 //Serve secure cookies in production
-if (app.get('env') === 'production')
-{
-  sess.cookie.secure = true
+if (app.get("env") === "production") {
+  sess.cookie.secure = true;
 }
 // Set session
-app.use(session(sess))
-
+app.use(session(sess));
 
 //-------------------------------------
 // On all req
-app.use('/', (req, res, next) => {
-  if (req.session.type === 'admin')
-  {
-    res.end('Welcome back, Big Chungus!')
+app.use("/", (req, res, next) => {
+  if (req.session.type === "admin") {
+    res.end("Welcome back, Big Chungus!");
   }
-  next()
-})
+  next();
+});
 
 //-------------------------------------
 // POST - Login
-app.use('/login', (req, res, next) => {
-  if (req.session.type === 'admin')
-  {
-    res.end('Welcome back, Big Chungus!')
-  }
-  else
-  {
-    const credentials = auth(req)
+app.use("/login", (req, res, next) => {
+  if (req.session.type === "admin") {
+    res.end("Welcome back, Big Chungus!");
+  } else {
+    const credentials = auth(req);
     if (!credentials || !check(credentials.name, credentials.pass)) {
-      res.statusCode = 401
-      res.setHeader('WWW-Authenticate', 'Basic realm="admin"')
-      console.log('Access denied!')
-      res.json({auth: false})
+      res.statusCode = 401;
+      res.setHeader("WWW-Authenticate", 'Basic realm="admin"');
+      console.log("Access denied!");
+      res.json({ auth: false });
     } else {
-      req.session.type="admin"
-      console.log(req.session.type)
-      console.log('Access granted!')
-      res.json({auth: true})
+      req.session.type = "admin";
+      console.log(req.session.type);
+      console.log("Access granted!");
+      res.json({ auth: true });
     }
   }
-  next()
-})
+  next();
+});
 
 // Basic function to validate credentials for example
-function check (name, pass) {
-  var valid = true
- 
+function check(name, pass) {
+  var valid = true;
+
   // Simple method to prevent short-circut and use timing-safe compare
-  valid = compare(name, process.env.ADMIN_USERNAME) && valid
-  valid = compare(pass, process.env.ADMIN_PASSWORD) && valid
-  return valid
+  valid = compare(name, process.env.ADMIN_USERNAME) && valid;
+  valid = compare(pass, process.env.ADMIN_PASSWORD) && valid;
+  return valid;
 }
 
 //-------------------------------------
 //  GET
-app.use('/', cloud)
+app.use("/", cloud);
 
 //-------------------------------------
 // POST
-app.post('/images', (req, res) => {
-  res.send("")
-})
+app.post("/images", (req, res) => {
+  res.send("");
+});
 
-app.put('/images', (req, res) => {
-  res.send("")
-})
+app.put("/images", (req, res) => {
+  res.send("");
+});
 
-app.delete('/images', (req, res) => {
-  res.send("")
-})
+app.delete("/images", (req, res) => {
+  res.send("");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
