c7cc3937d0d6e9695d9e1d7d3ffa9bcc6c9ac489
Kadence Neuens <kadenceneuens@gmail.com>
2020-09-24 01:47:16 -0500

    Finished tagging update route

--- routes/api.js
+++ routes/api.js
@@ -6,29 +6,77 @@ const router = require("express").Router();
 const cloudinary = require("../cloudinary/cloudinary");
 const { multiUploads } = require("../multer/multiUploads");
 
-// CLOUDINARY TAG RETRIEVE/ADD/REMOVE
+// CLOUDINARY TAG RETRIEVE & UPDATE
 
 // CLOUDINARY TAG RETRIEVE
 // -
 // Returns a json array of tags for given public_id(s)
 router.post("/tags", (req, res) => {
-    // if (!req.body.ids) throw new Error("You must include an array of public_ids!")
+    // Check if public_ids were supplied, then grab them
+    if (!req.body.ids) throw new Error("You must include an array of public_ids!");
     var ids = req.body.ids;
+    // init tags array to hold response for later
     var tags = [];
     cloudinary.v2.api.resources_by_id(
         ids,
         {tags: true},
         (error, result) => {
-            console.log(result);
+            console.log(result, error);
+            // Map through each resource returned and store resulting tags in tags
             result.resources.map((item) => {
                 tags.push(item.tags);
             });
+            // There is a 500 calls/hr limit to Admin API calls for Cloudinary under the free plan
             console.log("Admin API queries left: " + result.rate_limit_remaining + "/" + result.rate_limit_allowed);
             console.log("Rate resets hourly. Next reset: " + result.rate_limit_reset_at);
             res.json(tags);
         }
     )
 });
+
+// CLOUDINARY TAG UPDATE
+// -
+// (required) ids - an array of strings containing public_ids.
+// (optional) tags - an array of tags to add to the resources. If none are supplied, all tags will be removed from resources.
+router.put("/tags/update", (req,res) => {
+    // Check if public_ids were supplied, then grab them
+    if (!req.body.ids) throw new Error("You must include an array of public_ids!");
+    var ids = req.body.ids;
+    // If tags are supplied, add them after removing all tags, otherwise remove all tags.
+    if (req.body.tags)
+    {
+        var tags = req.body.tags;
+        cloudinary.v2.uploader.remove_all_tags(
+            ids,
+            (error,result) => {
+                // Map each tag to a add_tag() call for all resources
+                tags.map((item) => {
+
+                    cloudinary.v2.uploader.add_tag(
+                        item,
+                        ids,
+                        (error2,result2) => {
+                            console.log("Added tag: " + item + "...to: " + result2.public_ids, !error2 ? "" : error2);
+                        }
+                    )
+                    
+                })
+                res.status(200).json({ message: "Successfully updated tags"});
+            }
+        )
+    }
+    else // Remove all tags when none are supplied in input
+    {
+        cloudinary.v2.uploader.remove_all_tags(
+            ids,
+            (error, result) =>
+            {
+                console.log("Removed all tags from: " + result.public_ids, !error ? "" : error);
+                res.status(200).json({ essage: "Successfully removed all tags"});
+            })
+    }
+    
+})
 
 // CLOUDINARY IMAGE UPLOAD
 // -
@@ -72,7 +120,7 @@ router.delete("/delete", (req, res) => {
         data.map((item) => {
             cloudinary.v2.uploader.destroy(item);
         });
-        res.status(200).json({ essage: "Successfully deleted: " + {...data} });
+        res.status(200).json({ message: "Successfully deleted: " + {...data} });
     } catch(error) {
         res.status(422).json({ message: error.message });
     }
